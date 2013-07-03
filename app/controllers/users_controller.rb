dcacd9c4b378d7817f841cb82c6361f472a7f67e
James Bodenschatz <enevolaprime@gmail.com>
2013-07-03 15:23:57 -0400

    Finished user signup - end of chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
   
   def create
 	@user = User.new(params[:user])
 	if @user.save
-	  # handle save
+      flash[:success] = "Account created successfully! Welcome to the to-do list!"
+	  redirect_to @user
 	else
 	  render 'new'
 	end
+  end
+  
+  def show
+    # show only if it's the current user or if current user is an admin
+  
+    @user = User.find(params[:id])
+    if @user.nil?
+      render 'new'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 ToDoList::Application.routes.draw do
-  get "users/new"
-
   root to: 'static_pages#home'
   
+  # Users
+  resources :users
+  match '/signup',  to: 'users#new'
+  match '/users',	to: 'users#create' #maybe change this to /createuser?
+  
+  # Static pages
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
-  
-  match '/signup',  to: 'users#new'
-  
-  match '/users',	to: 'users#create'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
